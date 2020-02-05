0fa1e2ccb2811153be2795a6aa7747fb8477e6b2
PatrickO98 <patrick.oliveira98@live.com>
2020-02-05 00:42:46 -0300

    Add initial gulpfile configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,27 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+var path            = require("path");
+var gulp            = require('gulp');
+var uglify          = require('gulp-uglify');
+
+gulp.task('html', function() {
+    const filesToMove = [`development/*.html`, '!node_modules/**/*', '!dist/**/*'];
+    return gulp.src(filesToMove)
+            .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function() {
+    const filesToMove = [`development/images/*`];
+    return gulp.src(filesToMove)
+            .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('js', function () {
+    return gulp.src('development/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/assets/js'));
+});
+
+gulp.task('default', gulp.parallel('js', 'html', 'images'));
+
+
+gulp.task('dev', gulp.series('default', function () {}));
   
