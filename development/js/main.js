13abce702f914e68265713abd35e84c442799248
PatrickO98 <patrick.oliveira98@live.com>
2020-02-07 02:59:41 -0300

    Add content and style for media section

---
+++ development/js/main.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function(){
+    jQuery('.owl-carousel').owlCarousel({
+        items: 1,
+        loop: false,
+        nav: false,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 8000,
+        margin: 100
+    });
+});
